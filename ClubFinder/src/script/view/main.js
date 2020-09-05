4ce29d34276a3ea10f02f3b13f024b5d05531032
YaafiEA <yaafieffendi@gmail.com>
2020-09-05 15:05:38 +0700

    club-list & club-item component

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,9 +1,11 @@
+import '../component/club-list.js';
 import '../component/search-bar.js';
 import DataSource from '../data/data-source.js';
+// import clubs from '../data/clubs.js';
 
 const main = () => {
     const searchElement = document.querySelector("search-bar");
-    const clubListElement = document.querySelector("#clubList");
+    const clubListElement = document.querySelector("club-list");
 
     //ES6 ASYINCRONUS
     // const onButtonSearchClicked = () => {
@@ -23,29 +25,11 @@ const main = () => {
     };
 
     const renderResult = (results) => {
-        clubListElement.innerHTML = "";
-        results.forEach((club) => {
-            const {
-                name,
-                fanArt,
-                description
-            } = club;
-
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
-
-            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-                <div class="club-info">
-                    <h2>${name}</h2>
-                    <p>${description}</p>
-                </div>`;
-            clubListElement.appendChild(clubElement);
-        })
+        clubListElement.clubs = results;
     };
 
     const fallbackResult = (message) => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`
+        clubListElement.renderError(message);
     };
 
     searchElement.clickEvent = onButtonSearchClicked;
