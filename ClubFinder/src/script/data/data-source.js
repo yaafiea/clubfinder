42f7ed658e735ffb1291340d6ee8e00b96c79eeb
YaafiEA <yaafieffendi@gmail.com>
2020-09-07 02:03:08 +0700

    edit data-source to asycn await

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -1,7 +1,7 @@
 import clubs from './clubs.js';
 
 class DataSource {
-    static searchClub(keyword) {
+    static async searchClub(keyword) {
         // return new Promise((resolve, reject) => {
         //     const filteredClubs = clubs.filter((club) => {
         //         return club.name.toUpperCase().includes(keyword.toUpperCase());
@@ -14,17 +14,13 @@ class DataSource {
         //     }
         // })
 
-        return fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`)
-            .then(response => {
-                return response.json();
-            })
-            .then(responseJson => {
-                if (responseJson.teams) {
-                    return Promise.resolve(responseJson.teams);
-                } else {
-                    return Promise.reject(`${keyword} is not found`);
-                }
-            })
+        const response = await fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`);
+        const responseJson = await response.json();
+        if (responseJson.teams) {
+            return Promise.resolve(responseJson.teams);
+        } else {
+            return Promise.reject(`${keyword} is not found`);
+        }
     }
 }
 export default DataSource;
