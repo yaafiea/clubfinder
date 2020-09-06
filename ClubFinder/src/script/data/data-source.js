82449c80727fa2625458c6ea6076c3c94cfc97a3
YaafiEA <yaafieffendi@gmail.com>
2020-09-07 00:57:30 +0700

    fetch api

--- ClubFinder/src/script/data/data-source.js
+++ ClubFinder/src/script/data/data-source.js
@@ -2,18 +2,29 @@ import clubs from './clubs.js';
 
 class DataSource {
     static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter((club) => {
-                return club.name.toUpperCase().includes(keyword.toUpperCase());
-            });
+        // return new Promise((resolve, reject) => {
+        //     const filteredClubs = clubs.filter((club) => {
+        //         return club.name.toUpperCase().includes(keyword.toUpperCase());
+        //     });
 
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
-            }
-        })
+        //     if (filteredClubs.length) {
+        //         resolve(filteredClubs);
+        //     } else {
+        //         reject(`${keyword} is not found`);
+        //     }
+        // })
 
+        return fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(responseJson => {
+                if (responseJson.teams) {
+                    return Promise.resolve(responseJson.teams);
+                } else {
+                    return Promise.reject(`${keyword} is not found`);
+                }
+            })
     }
 }
 export default DataSource;
